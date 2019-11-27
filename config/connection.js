8c7af09c4d98c0d944ceb3d80fb94bf19a88903d
Sabrina <sabrina.muniz@icloud.com>
2019-11-26 19:35:16 -0500

    added Jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Salem2015",
-  database: "burger_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+ 
+if(process.env.JAWSDB_URL{
+  connection = mysql.createConnection(process.env.JAWSDB_URL);   
+}else{
+  connection: mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"Salem2015",
+    database: "burger_db"
+  });
+};
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
