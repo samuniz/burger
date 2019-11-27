5d572eeab271d8c23a230d66a8f3889890ecb554
Sabrina <sabrina.muniz@icloud.com>
2019-11-26 20:07:34 -0500

    added jawsdb credentials

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection;
- 
-if(process.env.JAWSDB_URL{
+
+if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);   
-}else{
-  connection: mysql.createConnection({
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password:"Salem2015",
--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ USE burger_db;
 -- Create the table tasks.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
